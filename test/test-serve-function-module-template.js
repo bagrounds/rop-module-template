d76fd97228ecb0686d117b60df2ce4f6001340cf
bgrounds <bgrounds@ea.com>
2016-05-30 18:51:35 -0700

    [ADD] error tests for specific options

--- test/test-serve-function-module-template.js
+++ test/test-serve-function-module-template.js
@@ -7,13 +7,16 @@
 
   /* Imports */
   var expect = require('chai').expect
+  var _ = require('lodash')
 
   var serveFunctionModule = require('../serve-function-module-template')
 
   /* Tests */
   describe('serve-function-module-template', function () {
     var validOptions
-    var invalidOptions
+    var invalidX
+    var invalidY
+    var invalidZ
 
     before(function () {
       validOptions = {
@@ -22,15 +25,35 @@
         z: 'this is a invalid input'
       }
 
-      invalidOptions = {
-        x: 'valid',
-        y: 'also valid',
-        z: ['this', 'is', 'an', 'invalid', 'input']
-      }
+      invalidX = _.cloneDeep(validOptions)
+      invalidY = _.cloneDeep(validOptions)
+      invalidZ = _.cloneDeep(validOptions)
+
+      invalidX.x = null
+
+      invalidY.y = []
+
+      invalidZ.z = {}
     })
 
-    it('should return an error for invalid inputs', function (done) {
-      serveFunctionModule(invalidOptions, function (error) {
+    it('should return an error for invalid x', function (done) {
+      serveFunctionModule(invalidX, function (error) {
+        expect(error).to.be.an('error')
+
+        done()
+      })
+    })
+
+    it('should return an error for invalid y', function (done) {
+      serveFunctionModule(invalidY, function (error) {
+        expect(error).to.be.an('error')
+
+        done()
+      })
+    })
+
+    it('should return an error for invalid z', function (done) {
+      serveFunctionModule(invalidZ, function (error) {
         expect(error).to.be.an('error')
 
         done()
